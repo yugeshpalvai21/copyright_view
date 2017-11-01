21ded10c32bb656c245eadef49f85158cdeaa4ad
yugeshreddypalvai <pyreddy517@hotmail.com>
2017-11-01 22:18:16 +0530

    copyright method updated

--- lib/copyright_view.rb
+++ lib/copyright_view.rb
@@ -2,7 +2,7 @@ require "copyright_view/version"
 
 module CopyrightView
   class Renderer
-  	def self.coyright start_year company_name message
+  	def self.coyright start_year, company_name, message
   		"Copyright &copy#{start_year}-#{Time.now.year} | <b>#{company_name}</b> | #{message}".html_safe
   	end
   end
