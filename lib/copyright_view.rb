f1e3c4163eda08a3f8961552912b4ff457572fd8
Yugesh Palvai <yugeshpalvai21@gmail.com>
2024-07-27 23:59:56 +0530

    updates logic for copyright_view with default values

--- lib/copyright_view.rb
+++ lib/copyright_view.rb
@@ -1,9 +1,14 @@
-require "copyright_view/version"
+require_relative "copyright_view/version"
 
 module CopyrightView
   class Renderer
-  	def self.copyright start_year, company_name, message
-  		"Copyright &copy#{start_year}-#{Time.now.year} | <b>#{company_name}</b> | #{message}".html_safe
-  	end
+    def self.copyright(start_year: nil, company_name: nil, message: nil)
+      str = <<~COPY
+        &copy;#{start_year ? "#{start_year}-" : ""}#{Time.now.year}    
+        #{company_name if company_name} 
+        #{message if message}
+      COPY
+      str.strip.split("\n").map(&:strip).join(" - ")
+    end
   end
 end
