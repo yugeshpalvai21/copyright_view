81b3d48e3b55112f5fd321eb1c9d6e7d1df4c359
Yugesh Palvai <yugeshpalvai21@gmail.com>
2024-07-28 00:05:05 +0530

    adds new test cases addressed updated logic

--- spec/copyright_view_spec.rb
+++ spec/copyright_view_spec.rb
@@ -3,7 +3,13 @@ RSpec.describe CopyrightView do
     expect(CopyrightView::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it "returns string with default values" do
+    copyright_response = CopyrightView::Renderer.copyright
+    expect(copyright_response).to eq "&copy;2024"
+  end
+
+  it "return proper response when given all the inputs" do
+    copyright_response = CopyrightView::Renderer.copyright(start_year: 2020, company_name: "Your Company", message: "All rights reserved.")
+    expect(copyright_response).to eq "&copy;2020-2024 - Your Company - All rights reserved."
   end
 end
